e97929414e44a6f02d52284081b8a019c14b1ac1
Yan Hong <onlyhongyan@gmail.com>
2017-11-04 15:42:17 -0700

    add color to chat

--- socket.js
+++ socket.js
@@ -1,9 +1,8 @@
 exports = module.exports = function(io) {
     io.on('connection', function(socket) {
         console.log('new connection!!!');
-        socket.on('chat message', function(m) {
-            console.log('message: ' +  m)
-            io.emit('chat broadcast', m)
+        socket.on('chat message', function(obj) {
+            io.emit('chat broadcast', obj)
         })
     })
 }
